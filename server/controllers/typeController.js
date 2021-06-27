5221fa46a5e56a168eecbc30941e5d4e30d362b4
ALX114 <58948125+ALX114@users.noreply.github.com>
2021-06-27 16:15:15 +0000

    replChange

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -3,11 +3,13 @@ const ApiError = require('../Error/apiError')
 
 class TypeController{
     async create(req, res){
-
+      const {name} = req.body
+      const type = await Type.create({name})
+      return res.json(type)
     }
     async getAll(req, res){
 
     }
 }
 
-module.exports =new TypeController()
+module.exports = new TypeController()
